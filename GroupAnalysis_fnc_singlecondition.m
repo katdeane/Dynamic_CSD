ff408fde74862446dd8d2c3cbf3766562944b86a
Katrina Deane <katrina.deane@lin-magdeburg.de>
2020-03-25 10:55:42 +0100

    added SinkINT parameter, removed all sorting of relres, absres, avrec

--- GroupAnalysis_fnc_singlecondition.m
+++ GroupAnalysis_fnc_singlecondition.m
@@ -26,10 +26,8 @@ entries = length(input);
 nThresh = 0.25; % Threshold for animals
 
 %cell of target output features
-para = { 'SinkRMS', 'tempSinkRMS', 'init_Peak_BF_tune', 'SinkDur',...
-    'Sinkonset', 'Sinkoffset','SinkPeakAmp','SinkPeakLate','Full_RMS_AVREC', ...
-    'Early_RMS_AVREC','Late_RMS_AVREC', 'Full_RMS_RELRES', 'Early_RMS_RELRES',...
-    'Late_RMS_RELRES','Full_RMS_ABSRES','Early_RMS_ABSRES','Late_RMS_ABSRES','IntCurFlow'};
+para = { 'SinkRMS', 'SinkINT', 'tempSinkRMS', 'init_Peak_BF_tune', 'SinkDur',...
+    'Sinkonset', 'Sinkoffset','SinkPeakAmp','SinkPeakLate','IntCurFlow'};
 singletrialpara = { 'SingleSinkPeakAmp','SingleSinkPeakLat','SingleSinkRMS'};
 
 SINKs = {'VbE', 'IVE', 'VIaE', 'VIbE', 'VaE', 'I_IIE','VbL', 'IVL', 'VIaL', 'VIbL', 'VaL', 'I_IIL'};
@@ -44,24 +42,10 @@ for i1 = 1:entries
     mkdir(['Group_' input(i1).name(1:end-9)]); %adds folder to directory
     
     names = fieldnames(Data); %list of animal names
-%     NumAnimals = length(names);
-    
-%     BF_Pos = [];
     DimDat = length(Data);
     
     %to determine where the BF should be placed (i.e. the length of the longest frequency list)
     BF_Pos = length(Data(1).(names{1}).Frqz);
-%     for iNum = 1:NumAnimals        
-%         for idim = 1:DimDat
-%             if ~isempty(Data(idim).(names{iNum}))
-%                 if isempty(BF_Pos)
-%                     BF_Pos = length(Data(idim).(names{iNum}).Frqz);
-%                 elseif length(Data(idim).(names{iNum}).Frqz) > BF_Pos
-%                     BF_Pos = length(Data(idim).(names{iNum}).Frqz);
-%                 end
-%             end
-%         end        
-%     end
     
     NumFreq = (BF_Pos*2)-1; %so that BF_Pos is center
     if BF_Pos == 8
@@ -72,7 +56,6 @@ for i1 = 1:entries
         bfposticks = {'not' 'acc' 'ur' 'ate' 'ly' 'tick' 'lab' 'eled'};
     end
     
-
     % Granular BF based
     [Data_GSBased,~] = Groupsorting(Data,names,para, DimDat,SINKs,NumFreq,'GS',mirror,nThresh,bin);
     %[sData_GSBased] = Groupsorting_single(Data,names,singletrialpara, DimDat,SINKs,NumFreq,'GS');
@@ -147,7 +130,6 @@ for i1 = 1:entries
     semVb = nanstd(Data_GSBased.(LTuning{it}).VbE,1)/sqrt(sum(~isnan(Vb)));
     semVIa = nanstd(Data_GSBased.(LTuning{it}).VIaE,1)/sqrt(sum(~isnan(VIa)));
 
-    
     subplot(1,length(Tuning),it)
     errorbar(IV,semIV,'LineWidth',2); hold on; 
     errorbar(Va,semVa,'LineWidth',2);
@@ -204,7 +186,6 @@ for i1 = 1:entries
     cd([homedir '\figs\' 'Group_' input(i1).name(1:end-9)]);
     savefig(h,[input(i1).name(1:end-4), ' Avg Tuning Curves ST']); close all
     
-    
     %% plot Latency Curves ST Avg
     LTuning = {'Sinkonset','SinkPeakLate'};
     h=figure('units','normalized','outerposition',[0 0 1 1], 'Name','Latency Curves GS');    
@@ -220,7 +201,6 @@ for i1 = 1:entries
     semVa = nanstd(Data_STBased.(LTuning{it}).VaE,1)/sqrt(sum(~isnan(Va)));
     semVb = nanstd(Data_STBased.(LTuning{it}).VbE,1)/sqrt(sum(~isnan(Vb)));
     semVIa = nanstd(Data_STBased.(LTuning{it}).VIaE,1)/sqrt(sum(~isnan(VIa)));
-
     
     subplot(1,length(Tuning),it)
     errorbar(IV,semIV,'LineWidth',2); hold on; 
@@ -296,74 +276,7 @@ for i1 = 1:entries
             savefig(h,[input(i1).name(1:end-4), ' rel Sink Tuning shifts towards granular layer ', FNames{i2}]); close all
         end
     end
-    %% plot relres and absres from GS based group
-    
-    %calculate mean from relres 
-    rrmean_full = nanmean(Data_GSBased.Full_RMS_RELRES,1);
-    rrmean_early = nanmean(Data_GSBased.Early_RMS_RELRES,1);
-    rrmean_late = nanmean(Data_GSBased.Late_RMS_RELRES,1);
-    
-    %calculate sem from relres 
-    rrsem_full = nanstd((Data_GSBased.Full_RMS_RELRES),[], 1)./ sqrt(sum(~isnan(Data_GSBased.Full_RMS_RELRES)));
-    rrsem_early = nanstd((Data_GSBased.Early_RMS_RELRES),[], 1)./ sqrt(sum(~isnan(Data_GSBased.Early_RMS_RELRES)));
-    rrsem_late = nanstd((Data_GSBased.Late_RMS_RELRES),[], 1)./ sqrt(sum(~isnan(Data_GSBased.Late_RMS_RELRES)));
  
-    
-    h=figure('units','normalized','outerposition',[0 0 1 1], 'Name','RelRes vs AbsRes');
-    
-    %%Relative Residual BF-2 (KD unbalanced sink and sources - intracortical temporal info)
-    subplot (1 ,5 ,1); hold on
-    title('RelRes low-far','FontSize',8,'FontWeight','bold');
-    bar([rrmean_early(BF_Pos-2),rrmean_late(BF_Pos-2),rrmean_full(BF_Pos-2)])
-    errorbar([1,2,3],[rrmean_early(BF_Pos-2),rrmean_late(BF_Pos-2),rrmean_full(BF_Pos-2)],[rrsem_early(BF_Pos-2),rrsem_late(BF_Pos-2),rrsem_full(BF_Pos-2)],'.','Color',[1 0 0])
-    set(gca,'XTick',1:1:3); set(gca,'XTickLabel',{'Early' 'Late' 'Full'},'FontSize',8);
-    axis([0 4 0 16])
-    ylabel('%','FontSize',8);
-    xlabel('Phase','FontSize',8);
-    
-    %Relative Residual BF-1
-    subplot (1 ,5 ,2); hold on
-    title('RelRes low-near','FontSize',8,'FontWeight','bold');
-    bar([rrmean_early(BF_Pos-1),rrmean_late(BF_Pos-1),rrmean_full(BF_Pos-1)])
-    errorbar([1,2,3],[rrmean_early(BF_Pos-1),rrmean_late(BF_Pos-1),rrmean_full(BF_Pos-1)],[rrsem_early(BF_Pos-1),rrsem_late(BF_Pos-1),rrsem_full(BF_Pos-1)],'.','Color',[1 0 0])
-    set(gca,'XTick',1:1:3); set(gca,'XTickLabel',{'Early' 'Late' 'Full'},'FontSize',8);
-    axis([0 4 0 16])
-    ylabel('%','FontSize',8);
-    xlabel('Phase','FontSize',8);
-    
-    %Relative Residual BF
-    subplot (1 ,5 ,3); hold on
-    title('RelRes BF','FontSize',8,'FontWeight','bold');
-    bar([rrmean_early(BF_Pos),rrmean_late(BF_Pos),rrmean_full(BF_Pos)])
-    errorbar([1,2,3],[rrmean_early(BF_Pos),rrmean_late(BF_Pos),rrmean_full(BF_Pos)],[rrsem_early(BF_Pos),rrsem_late(BF_Pos),rrsem_full(BF_Pos)],'.','Color',[1 0 0])
-    set(gca,'XTick',1:1:3); set(gca,'XTickLabel',{'Early' 'Late' 'Full'},'FontSize',8);
-    axis([0 4 0 16])
-    ylabel('%','FontSize',8);
-    xlabel('Phase','FontSize',8);
-    
-    %Relative Residual BF+1
-    subplot (1 ,5 ,4); hold on
-    title('RelRes high-near','FontSize',8,'FontWeight','bold');
-    bar([rrmean_early(BF_Pos+1),rrmean_late(BF_Pos+1),rrmean_full(BF_Pos+1)])
-    errorbar([1,2,3],[rrmean_early(BF_Pos+1),rrmean_late(BF_Pos+1),rrmean_full(BF_Pos+1)],[rrsem_early(BF_Pos+1),rrsem_late(BF_Pos+1),rrsem_full(BF_Pos+1)],'.','Color',[1 0 0])
-    set(gca,'XTick',1:1:3); set(gca,'XTickLabel',{'Early' 'Late' 'Full'},'FontSize',8);
-    axis([0 4 0 16])
-    ylabel('%','FontSize',8);
-    xlabel('Phase','FontSize',8);
-    
-    %Relative Residual BF+2
-    subplot (1 ,5 ,5); hold on
-    title('RelRes high-far','FontSize',8,'FontWeight','bold');
-    bar([rrmean_early(BF_Pos+2),rrmean_late(BF_Pos+2),rrmean_full(BF_Pos+2)])
-    errorbar([1,2,3],[rrmean_early(BF_Pos+2),rrmean_late(BF_Pos+2),rrmean_full(BF_Pos+2)],[rrsem_early(BF_Pos+2),rrsem_late(BF_Pos+2),rrsem_full(BF_Pos+2)],'.','Color',[1 0 0])
-    set(gca,'XTick',1:1:3); set(gca,'XTickLabel',{'Early' 'Late' 'Full'},'FontSize',8);
-    axis([0 4 0 16])
-    ylabel('%','FontSize',8);
-    xlabel('Phase','FontSize',8);
-    
-    cd([homedir '\figs\' 'Group_' input(i1).name(1:end-9)]);
-    savefig(h,[input(i1).name(1:end-4), ' RelRes']); close all
-    
     %% Save and Quit
     cd(homedir); cd DATA; clear Data;
     Data.GS_based = Data_GSBased;
